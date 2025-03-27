8e9ec169100a923c96f9508ce7a4d40e34817929
Matteodambr <Matteodambr@gmail.com>
2025-03-27 10:40:42 +0100

    Singularity checks updated to use conditioning of the jstar matrix.

--- src/kinematics_dynamics/singularity_check.m
+++ src/kinematics_dynamics/singularity_check.m
@@ -1,4 +1,4 @@
-function isSingular = singularity_check(H0, H0m, J0ee, Jmee, PARAM_Chaser)
+function isSingular = singularity_check(jstar, PARAM_Chaser)
 % SINGULARITY_CHECK Checks for arm singularity in a space manipulator.
 %
 %   isSingular = SINGULARITY_CHECK(H0, H0m, J0ee, Jmee, PARAM_Chaser)
@@ -23,19 +23,17 @@ function isSingular = singularity_check(H0, H0m, J0ee, Jmee, PARAM_Chaser)
 %   See also: Jstar, svd
 
 % Check for arm singularity:
-% On the Nature of Control Algorithms for Free-Floating Space Manipulators - Papadopoulos, Dubowsky
-% This reference says that singular configurations are found when det(J*)=0
-% As this is extremely inefficient to compute at each timestep, the check
-% on singularity is done by checking the minimum Singular Value (SV) of the matrix at each timestep.
-% Note: SVD is used since it works for non-square matrices, but maybe more efficient methods can be used???
-jstar = Jstar(H0, H0m, J0ee, Jmee) ;
-minSingularVal = min(svd(jstar)) ;
-distanceFromSingularity = minSingularVal - PARAM_Chaser.singularity.threshold ;
-isSingular = false ;
-if distanceFromSingularity < 0 
-    
-    % First condition: Singularity check on Jacobian, to check for ANY
-    % singularities
-    isSingular = true ;
+% NEW METHOD:
+%   +++   The manipulator singularity check is conducted using the
+%   +++   conditioning of jstar (cond = max_singular_value/min_singular_value),
+%   +++   to make sure that it does not become too large, indicating that
+%   +++   the matrix is ill-conditioned, and could cause numerical
+%   +++   instability.
+% DEPRECATED:
+%   ===   On the Nature of Control Algorithms for Free-Floating Space Manipulators - Papadopoulos, Dubowsky
+%   ===   This reference says that singular configurations are found when det(J*)=0
+%   ===   As this is extremely inefficient to compute at each timestep, the check
+%   ===   on singularity is done by checking the minimum Singular Value (SV) of the matrix at each timestep.
+%   ===   Note: SVD is used since it works for non-square matrices, but maybe more efficient methods can be used???
 
-end
+isSingular = ( cond(jstar) > PARAM_Chaser.singularity.thresholdConditioning ) ;
